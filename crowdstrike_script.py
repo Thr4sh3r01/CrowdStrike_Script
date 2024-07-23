1dad6d54a865a8d041c3c61980102981ec1dafa1
Thr4sh3r01 <thr4sh3r01@gmail.com>
2024-07-23 14:36:28 +0200

    añadidos comentarios

--- crowdstrike_script.py
+++ crowdstrike_script.py
@@ -1,12 +1,16 @@
 import os
 
 def eliminar_archivo_crowdstrike():
+    # Ruta donde se encuentran los archivos de CrowdStrike
     ruta_crowdstrike = r'C:\Windows\System32\drivers\CrowdStrike'
+    # Patrón de nombre de archivo a buscar
     nombre_archivo = 'C-00000291*.sys'
 
     try:
+        # Buscar archivos que coincidan con el patrón
         archivos_encontrados = [archivo for archivo in os.listdir(ruta_crowdstrike) if archivo.startswith(nombre_archivo)]
         if archivos_encontrados:
+            # Eliminar cada archivo encontrado
             for archivo in archivos_encontrados:
                 ruta_completa = os.path.join(ruta_crowdstrike, archivo)
                 os.remove(ruta_completa)
